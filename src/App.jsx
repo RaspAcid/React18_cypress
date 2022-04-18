57494ecb22359cf053c2c570495ee8a811be5a90
Olga Ilina <olga_ilina@epam.com>
2022-04-18 19:53:38 +0300

    create snapshot test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
 import React from 'react';
 import './App.css';
-import { DateView } from './components/Date/DateView';
 import { Greeting } from './components/Greeting/Greeting';
 
 export const App = () => {
   return (
     <div className="App" aria-label='App'>
       <Greeting/>
-      <DateView/>
     </div>
   );
 }
--- src/components/Greeting/Greeting.test.js
+++ src/components/Greeting/Greeting.test.js
@@ -1,10 +1,10 @@
-import { Greeting } from './Greeting';
 import renderer from 'react-test-renderer';
+import { Greeting } from './Greeting';
 
 test('renders greeting correctly', () => {
   jest.useFakeTimers();
   Date.now = jest.fn(() => 1789532048801);
 
-  const tree = renderer.create(<Greeting />).toJSON();
+  const tree = renderer.create(<Greeting />,).toJSON();
   expect(tree).toMatchSnapshot();
 });
